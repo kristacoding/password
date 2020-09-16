3b6de80f5137422070f065ecde949eccb6ed590f
Krista <krista.coding@gmail.com>
2020-09-16 10:47:56 -0400

    updated readme and javascript

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# password
+# Generate Password
+
+# Overview 
+    Using different if/else statements and functions, I created a webpage that will generate a password for the user based on a set criteria that it is given. The user is able to build a password between 8 and 128 characters with the option of using lowercase alphabet characters, uppercase alphabet characters, numbers and special characters. 
+
+# Description 
+    Page URL: https://kristacoding.github.io/password-generator/
+    Picture of Page: https://1drv.ms/u/s!AiHjcdKLh6idgwRYFlR80e0t7Ss7?e=l4XCdz
+
+    To generate the password, the page allows the user to click the 'generate password' button which leads them to muliple prompts to determine what they would like in their password. The prompts are created using different confirm and prompt tools from javascript. The password character count promote is then converted into a 'number', so that it can be used throughout the code to limit the length of the password. All the prompts are listed in the console log to be used a reference to confirm that page is reading all the selected options. 
+
+    
--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // Assignment Code
-var alphabetchoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var specialcharacter = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -15,35 +13,124 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-  //Generate password
-function generatePassword(){
-  var passwordlength = prompt("How many characters do you want in your password?");
-    if (passwordlength >= "8" || passwordlength <= "128"){}
-    else{
-      alert("You password must be between 8 and 128 characters");
-      return
-    }
-    var lowerCase = confirm("Do you want lowercase characters in your password?");
-    var upperCase = confirm("Do you want uppercase character in your password?");
-    var specialcharacter = confirm("Do you want special characters in your password?");
+//Generate password
+function generatePassword() {
+  var passwordlengthask = parseInt(prompt("How many characters do you want in your password?"));
+  //does not work if you enter 0
+  if (passwordlengthask >= 8 || passwordlengthask <= 128) {
+    console.log(passwordlengthask);
+    console.log(typeof (passwordlengthask));
+  }
+  else {
+    alert("You password must be between 8 and 128 characters");
+    return;
+  }
+  var number = confirm("Do you want numbers in your password?");
+  var lowerCase = confirm("Do you want lowercase characters in your password?");
+  var upperCase = confirm("Do you want uppercase character in your password?");
+  var specialcharacter = confirm("Do you want special characters in your password?");
 
-    if (lowerCase === true && upperCase === true && specialcharacter === true){
-      for (let i = 0; i < passwordlength.length; i++) {
 
-        
-        let password = alphabetchoices[math.floor(math.random()* loweralphatbet.length)];
-        let password =+ alphabetchoices.toUpperCase()[math.floor(math.random)*alphabetchoices.length];
-        let password =+ specialcharacter[math.floor(math.random()*specialcharacter.length)]
-        return (password)
-      }
-    }
+
+  //log in console to confirm we are getting the correct result
+  console.log(number);
+  console.log(lowerCase);
+  console.log(upperCase);
+  console.log(specialcharacter);
+  console.log(passwordlengthask);
+
+
+  var Loweralphabetchoices = ('abcdefghijklmnopqrstuvwxyz').split("");
+  var Upperalphabetchoices = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split("");
+  var numberchoices = ('0,1,2,3,4,5,6,7,8,9');
+  var specialcharacterchoices = ('!#$%&()*+,-./:;<=>?@[\]^_`{|}').split("");
+  //var password = "";
+  var choices = "";
+
+  // if no prompts are selected, returns alert
+  if (number === false && lowerCase === false && upperCase === false && specialcharacter === false) {
+    alert("Can not provide password - Must select at least one type of character!");
+    return;
+  }
+  // depending on answers picked create a string with all possible characters
+  else if (!number && !lowerCase && !upperCase && !specialcharacter) {
+    choices = numberchoices.concat(Loweralphabetchoices, Upperalphabetchoices, specialcharacterchoices);
   }
 
+  // three possible answers
 
+  else if (!number && !lowerCase && !upperCase){
+    choices = numberchoices.concat(Loweralphabetchoices, Upperalphabetchoices);
+  }
 
+  else if (number && lowerCase && specialcharacterchoices) {
+    choices = numberchoices.concat(Loweralphabetchoices, specialcharacterchoices);
+  }
 
+  else if(number && upperCase && specialcharacterchoices){
+    choices = numberchoices.concat(Upperalphabetchoices, specialcharacterchoices);
+  }
 
+  else if(lowerCase && upperCase && specialcharacterchoices){
+    choices = Loweralphabetchoices.concat(Upperalphabetchoices, specialcharacterchoices);
+  }
 
+  // two possible answers
 
+  else if(number && lowerCase){
+    choices = numberchoices.concat(Loweralphabetchoices);
+  }
 
+  else if(number && upperCase){
+    choices = numberchoices.concat(Upperalphabetchoices);
+  }
 
+  else if(number && specialcharacter){
+    choices = numberchoices.concat(specialcharacterchoices);
+  }
+
+  else if(lowerCase && upperCase){
+    choices = Loweralphabetchoices.concat(Upperalphabetchoices);
+  }
+
+  else if(lowerCase && specialcharacter){
+    choices = Loweralphabetchoices.concat(specialcharacterchoices);
+  }
+
+  else if(upperCase && specialcharacter){
+    choices = Upperalphabetchoices.concat(specialcharacterchoices);
+  }
+
+  // if one possible answer
+
+  else if(number){
+    choices = numberchoices;
+  }
+
+  else if(lowerCase){
+    choices = Loweralphabetchoices;
+  }
+
+  else if(upperCase){
+    choices = Upperalphabetchoices;
+  }
+
+  else if(specialcharacterchoices){
+    choices = specialcharacterchoices;
+  }
+
+  console.log(choices)
+
+  //building the password
+
+for (let i = 0; i < passwordlengthask; i++) {
+  var password = choices[Math.floor(Math.random() * choices.lenght)];
+  return password
+}
+
+//convert to a string
+
+//var stringpassword = password.join("");
+//return stringpassword;
+
+}
