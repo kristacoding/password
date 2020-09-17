89d8989c73dcd65f8896b51157338ac4327e63b1
Krista <krista.coding@gmail.com>
2020-09-16 21:24:36 -0400

    final push

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //Generate password
-function generatePassword() {
-  var passwordlengthask = parseInt(prompt("How many characters do you want in your password?"));
+function buildPassword() {
+  var passwordLengthAsk = parseInt(prompt("How many characters do you want in your password?"));
   //does not work if you enter 0
-  if (passwordlengthask >= 8 || passwordlengthask <= 128) {
-    console.log(passwordlengthask);
-    console.log(typeof (passwordlengthask));
+  if (passwordLengthAsk >= 8 && passwordLengthAsk <= 128) {
+    console.log(passwordLengthAsk);
+    console.log(typeof (passwordLengthAsk));
   }
   else {
     alert("You password must be between 8 and 128 characters");
@@ -28,109 +28,81 @@ function generatePassword() {
   var number = confirm("Do you want numbers in your password?");
   var lowerCase = confirm("Do you want lowercase characters in your password?");
   var upperCase = confirm("Do you want uppercase character in your password?");
-  var specialcharacter = confirm("Do you want special characters in your password?");
+  var specialCharacter = confirm("Do you want special characters in your password?");
 
-
-
-  //log in console to confirm we are getting the correct result
-  console.log(number);
-  console.log(lowerCase);
-  console.log(upperCase);
-  console.log(specialcharacter);
-  console.log(passwordlengthask);
-
-
-  var Loweralphabetchoices = ('abcdefghijklmnopqrstuvwxyz').split("");
-  var Upperalphabetchoices = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split("");
-  var numberchoices = ('0,1,2,3,4,5,6,7,8,9');
-  var specialcharacterchoices = ('!#$%&()*+,-./:;<=>?@[\]^_`{|}').split("");
-  //var password = "";
-  var choices = "";
+    //log in console to confirm we are getting the correct result
+    console.log(number);
+    console.log(lowerCase);
+    console.log(upperCase);
+    console.log(specialCharacter);
+    console.log(passwordLengthAsk);
 
   // if no prompts are selected, returns alert
-  if (number === false && lowerCase === false && upperCase === false && specialcharacter === false) {
+  if (number === false && lowerCase === false && upperCase === false && specialCharacter === false) {
     alert("Can not provide password - Must select at least one type of character!");
     return;
   }
-  // depending on answers picked create a string with all possible characters
-  else if (!number && !lowerCase && !upperCase && !specialcharacter) {
-    choices = numberchoices.concat(Loweralphabetchoices, Upperalphabetchoices, specialcharacterchoices);
-  }
 
-  // three possible answers
-
-  else if (!number && !lowerCase && !upperCase){
-    choices = numberchoices.concat(Loweralphabetchoices, Upperalphabetchoices);
-  }
-
-  else if (number && lowerCase && specialcharacterchoices) {
-    choices = numberchoices.concat(Loweralphabetchoices, specialcharacterchoices);
-  }
-
-  else if(number && upperCase && specialcharacterchoices){
-    choices = numberchoices.concat(Upperalphabetchoices, specialcharacterchoices);
-  }
-
-  else if(lowerCase && upperCase && specialcharacterchoices){
-    choices = Loweralphabetchoices.concat(Upperalphabetchoices, specialcharacterchoices);
-  }
-
-  // two possible answers
-
-  else if(number && lowerCase){
-    choices = numberchoices.concat(Loweralphabetchoices);
-  }
-
-  else if(number && upperCase){
-    choices = numberchoices.concat(Upperalphabetchoices);
-  }
-
-  else if(number && specialcharacter){
-    choices = numberchoices.concat(specialcharacterchoices);
-  }
-
-  else if(lowerCase && upperCase){
-    choices = Loweralphabetchoices.concat(Upperalphabetchoices);
-  }
-
-  else if(lowerCase && specialcharacter){
-    choices = Loweralphabetchoices.concat(specialcharacterchoices);
-  }
-
-  else if(upperCase && specialcharacter){
-    choices = Upperalphabetchoices.concat(specialcharacterchoices);
-  }
-
-  // if one possible answer
-
-  else if(number){
-    choices = numberchoices;
-  }
-
-  else if(lowerCase){
-    choices = Loweralphabetchoices;
-  }
-
-  else if(upperCase){
-    choices = Upperalphabetchoices;
-  }
-
-  else if(specialcharacterchoices){
-    choices = specialcharacterchoices;
-  }
-
-  console.log(choices)
-
-  //building the password
-
-for (let i = 0; i < passwordlengthask; i++) {
-  var password = choices[Math.floor(Math.random() * choices.lenght)];
-  return password
+  //Object holding password options
+  var passwordOptions = {
+    length: passwordLengthAsk,
+    number: number,
+    lowerCase: lowerCase,
+    upperCase: upperCase,
+    specialcharacter: specialCharacter
+  };
+  return passwordOptions;
 }
 
-//convert to a string
 
-//var stringpassword = password.join("");
-//return stringpassword;
+function generatePassword() {
+  //will run through writePassword function
+  var options = buildPassword();
+  
+  var lowerAlphabetChoices = ('abcdefghijklmnopqrstuvwxyz').split("");
+  console.log(lowerAlphabetChoices); //check that it is returning an array
+  var upperAlphabetChoices = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split("");
+  var numberChoices = ('0,1,2,3,4,5,6,7,8,9').split("");
+  var specialCharacterChoices = ('!#$%&()*+,-./:;<=>?@[\]^_`{|}').split("");
+  var password = [];//equals an array
+  var possibleCharacters = [];
+  var choices = [];
 
+  if (options.specialCharacter){
+    possibleCharacters = possibleCharacters.concat(specialCharacterChoices);
+    choices.push(randomletters(specialCharacterChoices));
+  }
+
+  if (options.lowerCase){
+    possibleCharacters = possibleCharacters.concat(lowerAlphabetChoices);
+    choices.push(randomletters(lowerAlphabetChoices));
+  }
+
+  if (options.upperCase){
+    possibleCharacters = possibleCharacters.concat(upperAlphabetChoices);
+    choices.push(randomletters(upperAlphabetChoices));
+  }
+
+  if (options.number){
+    possibleCharacters = possibleCharacters.concat(numberChoices);
+    choices.push(randomletters(numberChoices));
+  }
+
+  //all the 71 -73 need to before
+  for (let i = 0; i < options.length; i++) {
+    var possibleCharacter = randomletters(possibleCharacters);
+    password.push(possibleCharacter);   
+  }
+
+  for (let i = 0; i < choices.length; i++) {
+    password[i] = choices[i];   
+  }
+  
+  return password.join("");
+
+  function randomletters(array){
+    var randomIndex = Math.floor(Math.random() * array.length);
+    var randElement = array[randomIndex];
+    return randElement    
+  }
 }
